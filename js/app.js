bd3cd7ad8578d1f480f5065333dac08bfb47c09a
Kamil <kamilpiatkowski@gmail.com>
2017-04-01 16:34:36 +0200

    Added  custom and random options for board

--- js/app.js
+++ js/app.js
@@ -1,25 +1,48 @@
 (function () {
+
     // Elements
     var chessboard = document.querySelector("#chessboard");
 
     // Buttons
     var makeBoardButton = document.querySelector("#makeBoardButton");
-    var setupBoardButton = document.querySelector("#setupBoardButton");
-
+    var normalBoardButton = document.querySelector("#normalBoardButton");
+    var randomBoardButton = document.querySelector("#randomBoardButton");
+    var setBoardSizeButton = document.querySelector("#setBoardSize");
+    
+    // do usuniecia w przyszłości
     makeBoardButton.onclick = function () {
         makeBoard();
     }
 
-    setupBoardButton.onclick = function () {
-        setupBoard();
+    normalBoardButton.onclick = function () {
+        normalBoard();
+    }
+
+    randomBoardButton.onclick = function () {
+        randomBoard();
+    }
+    
+    setBoardSizeButton.onclick = function () {
+        makeBoard();
     }
 
     // Variables
     var boardSize = 8;
-    var piecesNumber = 32;
+    var piecesNumber = 10;
 
     // Functions
+
     function makeBoard() {
+        
+        var txt = "";
+        var boardSize = document.querySelector("#boardsize").value;
+        if (isNaN(boardSize) || boardSize < 1 || boardSize > 20) {
+            txt = "Input not valid";
+        } else {
+            txt = "Input OK";
+        }
+        document.querySelector("#boardsizetxt").innerHTML = txt;
+        
         chessboard.innerHTML = "";
         chessboard.style = "width: " + boardSize * 80 + "px; height: " + boardSize * 80 + "px;";
         var boardArray = new Array(boardSize);
@@ -42,25 +65,73 @@
         console.log("TEST: Make Board!");
     }
 
-    function setupBoard() {
+    function normalBoard() {
+        makeBoard();
+        var normalPiecesArray = [BRook1, BKnight1, BBishop1, BKing, BQueen, BBishop2, BKnight2, BRook2, BPawn1, BPawn2, BPawn3, BPawn4, BPawn5, BPawn6, BPawn7, BPawn8, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , WPawn1, WPawn2, WPawn3, WPawn4, WPawn5, WPawn6, WPawn7, WPawn8, WRook1, WKnight1, WBishop1, WKing, WQueen, WBishop2, WKnight2, WRook2];
+        console.log(normalPiecesArray);
         var squares = chessboard.querySelectorAll(".square");
-        for(let i = 0; i < squares.length; i++) {
-            squares[i].innerHTML = WBishop1.img;
-            squares[i].setAttribute("draggable", "true");
-            squares[i].onclick = function() {
-                displayMoves();
+        for (let i = 0; i < squares.length; i++) {
+            if (normalPiecesArray[i] instanceof ChessPiece) {
+                squares[i].innerHTML = normalPiecesArray[i].img;
+                squares[i].onclick = function () {
+                    displayMoves();
+                }
+            } else {
+                squares[i].innerHTML = "";
             }
+            squares[i].setAttribute("draggable", "true");
         }
-        console.log("TEST: Setup Board!");
-        return squares;
     }
 
     function randomBoard() {
+        makeBoard();
+        var txt = "";
+        var piecesNumber = document.querySelector("#piecesnumber").value;
+        if (isNaN(piecesNumber) || piecesNumber < 0 || piecesNumber > 32) {
+            txt = "Input not valid";
+        } else {
+            txt = "Input OK";
+        }
+        document.querySelector("#piecesnumbertxt").innerHTML = txt;
+        var armyNumber = piecesNumber / 2;
+        var whiteArmy = [WPawn1, WPawn2, WPawn3, WPawn4, WPawn5, WPawn6, WPawn7, WPawn8, WRook1, WKnight1, WBishop1, WKing, WQueen, WBishop2, WKnight2, WRook2];
+        var blackArmy = [BRook1, BKnight1, BBishop1, BKing, BQueen, BBishop2, BKnight2, BRook2, BPawn1, BPawn2, BPawn3, BPawn4, BPawn5, BPawn6, BPawn7, BPawn8];
+        var whiteRandomArmy = new Array(armyNumber);
+        var blackRandomArmy = new Array(armyNumber);
+        for (let i = 0; i < armyNumber; i++) {
+            whiteRandomArmy[i] = whiteArmy[Math.floor(Math.random() * whiteArmy.length)];
+            blackRandomArmy[i] = blackArmy[Math.floor(Math.random() * blackArmy.length)];
+        }
+        var emptyRandomArray = new Array(boardSize * boardSize - whiteRandomArmy.length - blackRandomArmy.length);
+        for (let i = 0; i < emptyRandomArray.length; i++) {
+            emptyRandomArray[i] = null;
+        }
+        var randomPiecesArray = whiteRandomArmy.concat(blackRandomArmy).concat(emptyRandomArray);
+        shuffle(randomPiecesArray);
 
+        var squares = chessboard.querySelectorAll(".square");
+        for (let i = 0; i < squares.length; i++) {
+            if (randomPiecesArray[i] instanceof ChessPiece) {
+                squares[i].innerHTML = randomPiecesArray[i].img;
+                squares[i].onclick = function () {
+                    displayMoves();
+                }
+            } else {
+                squares[i].innerHTML = "";
+            }
+            squares[i].setAttribute("draggable", "true");
+        }
     }
 
-    function displayMoves() {
+    function shuffle(array) {
+        for (let i = array.length; i; i--) {
+            let j = Math.floor(Math.random() * i);
+        [array[i - 1], array[j]] = [array[j], array[i - 1]];
+        }
+    }
 
+    function displayMoves(e) {
+        console.log("TEST: Piece clicked! " + e);
     }
 
     // Pieces
@@ -73,8 +144,9 @@
         }
     }
 
-    var whiteArmy = [WKing, WQueen, WBishop1, WBishop2, WKnight1, WKnight2, WRook1, WRook2, WPawn1, WPawn2, WPawn3, WPawn4, WPawn5, WPawn6, WPawn7, WPawn8];
-    var blackArmy = [BKing, BQueen, BBishop1, BBishop2, BKnight1, BKnight2, BRook1, BRook2, BPawn1, BPawn2, BPawn3, BPawn4, BPawn5, BPawn6, BPawn7, BPawn8];
+    var whiteArmy = [WPawn1, WPawn2, WPawn3, WPawn4, WPawn5, WPawn6, WPawn7, WPawn8, WRook1, WKnight1, WBishop1, WKing, WQueen, WBishop2, WKnight2, WRook2];
+    var blackArmy = [BRook1, BKnight1, BBishop1, BKing, BQueen, BBishop2, BKnight2, BRook2, BPawn1, BPawn2, BPawn3, BPawn4, BPawn5, BPawn6, BPawn7, BPawn8];
+    var emptyArray = [, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , ];
 
     var WKing = new ChessPiece("WKing", "white", '<img src="img/white_king.png">', "king");
     var WQueen = new ChessPiece("WQueen", "white", '<img src="img/white_queen.png">', "queen");
@@ -93,7 +165,7 @@
     var WPawn7 = new ChessPiece("WPawn7", "white", '<img src="img/white_pawn.png">', "pawn");
     var WPawn8 = new ChessPiece("WPawn8", "white", '<img src="img/white_pawn.png">', "pawn");
 
-    var BKing = new ChessPiece("BKing", "white", '<img src="img/black_king.png">', "king");
+    var BKing = new ChessPiece("BKing", "white", '<img id="BKing" class="king" player="white" src="img/black_king.png">', "king");
     var BQueen = new ChessPiece("BQueen", "white", '<img src="img/black_queen.png">', "queen");
     var BBishop1 = new ChessPiece("BBishop1", "white", '<img src="img/black_bishop.png">', "bishop");
     var BBishop2 = new ChessPiece("BBishop2", "white", '<img src="img/black_bishop.png">', "bishop");
@@ -109,6 +181,5 @@
     var BPawn6 = new ChessPiece("BPawn6", "white", '<img src="img/black_pawn.png">', "pawn");
     var BPawn7 = new ChessPiece("BPawn7", "white", '<img src="img/black_pawn.png">', "pawn");
     var BPawn8 = new ChessPiece("BPawn8", "white", '<img src="img/black_pawn.png">', "pawn");
-
 
 })();
